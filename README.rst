755fec7eabd52f1c0440b819a0313eb0178928d1
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-24 15:48:34 +0900

    initial commit

---
+++ README.rst
@@ -0,0 +1,45 @@
+python-jsmapper
+===============
+|travis|_ |coveralls|_ |pypibadge|_
+
+-----
+About
+-----
+A Object - JSON Schema Mapper.
+
+-------
+Install
+-------
+
+.. code:: shell
+
+  $ pip install jsmapper
+
+Or
+
+.. code:: shell
+
+  $ python setup.py install
+
+-------------
+Run the tests
+-------------
+
+.. code:: shell
+
+  $ tox
+
+-------
+License
+-------
+python-jsmapper is licensed under the MIT LICENSE.  See ./LICENSE.rst.
+
+
+.. _travis: https://travis-ci.org/yosida95/python-jsmapper
+.. |travis| image:: https://travis-ci.org/yosida95/python-jsmapper.svg?branch=master
+
+.. _coveralls: https://coveralls.io/r/yosida95/python-jsmapper?branch=master
+.. |coveralls| image:: https://coveralls.io/repos/yosida95/python-jsmapper/badge.png?branch=master
+
+.. _pypibadge: http://badge.fury.io/py/jsmapper
+.. |pypibadge| image:: https://badge.fury.io/py/jsmapper.svg?dummy
---
+++ setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.dirname(__file__)
+requires = [
+    'jsonschema',
+]
+tests_require = [
+    'nose',
+    'coverage'
+]
+
+
+def _read(name):
+    try:
+        return open(os.path.join(here, name)).read()
+    except:
+        return ""
+readme = _read("README.rst")
+license = _read("LICENSE.rst")
+
+setup(
+    name='jsmapper',
+    version='0.0.1',
+    test_suite='jsmapper',
+    author='Kohei YOSHIDA',
+    author_email='license@yosida95.com',
+    description='JSON Web Token library for Python 3.',
+    long_description=readme,
+    license=license,
+    url='https://github.com/yosida95/python-jsmapper',
+    packages=find_packages(),
+    install_requires=requires,
+    tests_require=tests_require,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
