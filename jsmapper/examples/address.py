18c8064a842278537f0e097428fafad325b3e6ee
yosida95 <kohei@yosida95.com>
2014-04-26 15:11:42 +0900

    follow new spec

--- jsmapper/examples/address.py
+++ jsmapper/examples/address.py
@@ -17,36 +17,36 @@ from jsmapper.types import (
 class Address(Mapping):
 
     @object_property(name='post-office-box')
-    def post_office_box(self):
+    def post_office_box():
         return JSONSchema(String())
 
     @object_property(name='extended-address')
-    def extended_address(self):
+    def extended_address():
         return JSONSchema(String())
 
     @object_property(name='street-address')
-    def street_address(self):
+    def street_address():
         return JSONSchema(String())
 
     locality = JSONSchema(String())
     region = JSONSchema(String())
 
     @object_property(name='postal-code')
-    def postal_code(self):
+    def postal_code():
         return JSONSchema(String())
 
     @object_property(name='country-name')
-    def country_name(self):
+    def country_name():
         return JSONSchema(String())
 
 
 AddressSchema = JSONSchema(
     type=Object(
         properties=Address,
-        dependencies=[
-            (Address.post_office_box, Address.street_address),
-            (Address.extended_address, Address.street_address),
-        ],
+        dependencies={
+            Address.post_office_box: Address.street_address,
+            Address.extended_address: Address.street_address,
+        },
         required=[Address.locality, Address.region],
     ),
     description="An Address following the convention of "
