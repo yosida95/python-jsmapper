9f035443155f8b7ca9a8f5e12209d12459bdfe41
yosida95 <kohei@yosida95.com>
2014-04-26 17:13:46 +0900

    fix testcases

--- jsmapper/tests/__init__.py
+++ jsmapper/tests/__init__.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import unittest
-
-from ..examples import address
-
-
-class TestMapping(unittest.TestCase):
-    address_request = {
-        'country-name': 'Japan',
-        'region': 'Tokyo',
-        'locality': 'Chiyoda-ku',
-        'street-address': '1-3-6 Kudan-kita',
-        'extended-address': '7F',
-    }
-
-    def test_bind_address(self):
-        inst = address.Address._bind(self.address_request)
-        self.assertIsInstance(inst, address.Address)
-        self.assertEqual(inst.country_name, 'Japan')
-        self.assertEqual(inst.region, 'Tokyo')
-        self.assertEqual(inst.locality, 'Chiyoda-ku')
-        self.assertEqual(inst.street_address, '1-3-6 Kudan-kita')
-        self.assertEqual(inst.extended_address, '7F')
-
-        self.assertIsNone(inst.postal_code)
-        self.assertIsNone(inst.post_office_box)
+address_request = {
+    'country-name': 'Japan',
+    'region': 'Tokyo',
+    'locality': 'Chiyoda-ku',
+    'street-address': '1-3-6 Kudan-kita',
+    'extended-address': '7F',
+}
---
+++ jsmapper/tests/test_types.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from . import address_request
+from ..examples import address
+from ..types import Object
+
+
+class TestPrimitiveType(unittest.TestCase):
+
+    def test_object(self):
+        obj = Object(properties=address.Address)
+        inst = obj.bind(address_request)
+
+        self.assertIsInstance(inst, address.Address)
+        self.assertEqual(inst.country_name, 'Japan')
+        self.assertEqual(inst.region, 'Tokyo')
+        self.assertEqual(inst.locality, 'Chiyoda-ku')
+        self.assertEqual(inst.street_address, '1-3-6 Kudan-kita')
+        self.assertEqual(inst.extended_address, '7F')
+
+        self.assertIsNone(inst.postal_code)
+        self.assertIsNone(inst.post_office_box)
+
+    def test_object_without_properties(self):
+        obj = Object()
+        inst = obj.bind(address_request)
+
+        self.assertIs(inst, address_request)
